e666271485a14a3e2d23d1780872252dfa3e7b1b
EderPaulo <paulofiuza24@gmail.com>
2024-10-07 15:42:45 -0300

    Exemplo  simples para calcular juros simples

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿CalculoDoJuros();
+static void CalculoDoJuros()
+{
+    Console.Clear();
+
+    Console.Write("Informe o valor do capital inicial: ");
+    double capital = double.Parse(Console.ReadLine());
+
+    Console.Write("Informe a taxa de juros: ");
+    double taxaJuros = double.Parse(Console.ReadLine());
+
+    Console.Write("Informe o tempo da aplicação em meses: ");
+    double tempoMeses = double.Parse(Console.ReadLine());
+
+    double montanteFinal = ((taxaJuros / 1000) * capital * tempoMeses) + capital;
+
+    Console.WriteLine(montanteFinal);
+}
